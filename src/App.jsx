56484ebd43a3bc7d93440f4c68d647aec52d5ca1
RanungPark <ckehfqkr302@naver.com>
2023-06-10 19:44:48 +0900

    filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList/TodoList';
+import TodoHeader from './TodoHeader/TodoHeader';
+
+const filters = ['all', 'active', 'completed']
 
 export default function App() {
+  const [filter, setFilter] = useState(filters[0]);
+
   return (
     <div>
-      <TodoList />
+      <TodoHeader filters={filters} filter={filter} onFilterChange={setFilter}/>
+      <TodoList filter={filter}/>
     </div>
   );
 }
