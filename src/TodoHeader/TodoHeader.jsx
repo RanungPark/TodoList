83a9d729af3b9f3dab488e015e25d3dda48a8896
RanungPark <ckehfqkr302@naver.com>
2023-06-14 13:12:51 +0900

    style header

--- src/TodoHeader/TodoHeader.jsx
+++ src/TodoHeader/TodoHeader.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './TodoHeader.module.css'
 
 export default function TodoHeader({filters, filter, onFilterChange}) {
 
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {
           filters.map((value, index) => (
             <li key={index}>
-              <button onClick={()=> onFilterChange(value)}>{value}</button>
+              <button className={`${styles.filter} ${filter === value && styles.selected}`} onClick={()=> onFilterChange(value)}>{value}</button>
             </li>
           ))
         }
